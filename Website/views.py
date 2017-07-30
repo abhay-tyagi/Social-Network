1f5f765378ada48108c0ccbe40d19b5668568ee5
Abhay <abhaytyagi1998@gmail.com>
2017-07-30 16:01:08 +0530

    Imports

--- Website/views.py
+++ Website/views.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment, Like, User
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
-    temp_context = {
-        "posts": "hello"
-    }
-    return render(request, 'Website/index.html', temp_context)
+    
+    return render(request, 'Website/index.html', {"posts": "hello"})
