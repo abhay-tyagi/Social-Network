991267cb72d58c9ef6ea112de85043ca2591528d
Abhay <abhaytyagi1998@gmail.com>
2017-08-07 02:09:44 +0530

    Bug fixes

--- Website/urls.py
+++ Website/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^show_profile/$', views.show_profile, name='show_profile'),
 ]
